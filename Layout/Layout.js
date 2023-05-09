424f3109933acd42612bf50d24d988f90f191927
Afridi <90641219+SerialChillar@users.noreply.github.com>
2023-05-09 22:52:04 +0600

    2nd commit

---
+++ Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react' ;
+import Footer from './Footer';
+import Header from './Header';
+import  { Toaster } from 'react-hot-toast';
+
+const Layout = ({children}) =>{
+    return(
+        <div>
+          <Header/>
+          <main>
+          <Toaster />
+           {children}
+          </main>
+          <Footer/>
+        </div>
+    );
+};
+export default Layout ;
