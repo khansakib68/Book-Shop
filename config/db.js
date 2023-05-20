28f5518a98a7547d97abcbc5862368563c341bbe
khansakib68 <128209198+khansakib68@users.noreply.github.com>
2023-05-20 20:59:42 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import colors from 'colors';
 
 const connectDB = async() => {
     try{
-        const conn =await mongoose.connect(process.env.MONGO_URL) ; 
+        const conn =await mongoose.connect(process.env.MONGO_URL) || (process.env.RENDER_URL) ; 
        
     } catch (error) {
         
